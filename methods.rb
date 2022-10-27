78b804d6cdc0615be44f4679cae3e85c9eeaccc9
Colleen Cabedo <cmcabedo@gmail.com>
2022-10-27 15:04:54 -0400

    commit tree 8c0581a5571cb99c85a31c0324fe0d84f71a3110

--- methods.rb
+++ methods.rb
@@ -1,1 +1,15 @@
-# Your code here!
+def greet_programmer()
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default 
